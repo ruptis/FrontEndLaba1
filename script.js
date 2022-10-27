79cb35750ebe4d9e787054f1340a329a6fba5583
Nikmoofte <ilya.vailov@gmail.com>
2022-10-27 13:55:01 +0300

    added modal window

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ document.querySelector('.header_burger').onclick = function() {
   document.querySelector('.header_content').classList.toggle('active');
   document.querySelector('.header').classList.toggle('active');
 }
+
+document.getElementById("close").onclick = function()
+{
+    const modal = document.getElementById("openModal");
+    modal.classList.toggle('active');
+}
+
+function sbmt(f)
+    {
+        const modal = document.getElementById("openModal");
+        modal.classList.add("active");
+    }
+                                                    
